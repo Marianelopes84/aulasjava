734c7c1b82bb3a24fdf1f0ccec3c4bcf21fc2ee8
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-03-16 23:06:07 +0000

    teste

---
+++ TesteSwitchCase2.java
@@ -0,0 +1,9 @@
+public class TesteSwitchCase2 {
+    public static void main (String[] args) {
+        int vet[] = new int[3];
+        vet[0] = 0;
+        vet[1] = 10;
+        vet[2] = 20;
+        System.out.println(vet[0] + " "+vet[1]+ " "+vet[2]);
+    }
+}
