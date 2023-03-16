977b987881bac1fd1f6ab998b20c65bc93a30951
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-03-16 22:57:50 +0000

    teste

--- exscanner.java
+++ exscanner.java
@@ -3,20 +3,20 @@ public class exscanner
 {
     public static void main (String[] args)
     {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan1 = new Scanner(System.in);
         String nome, telefone, rua, numero;
 
         System.out.print("Digite o seu nome:");
-        nome = scan.nextLine();
+        nome = scan1.nextLine();
      
         System.out.print("Digite o seu telefone:");
-        telefone = scan.nextLine();
+        telefone = scan1.nextLine();
 
         System.out.print("Digite sua rua:");
-        rua = scan.nextLine();
+        rua = scan1.nextLine();
 
         System.out.print("Digite o numero da sua casa:");
-        numero = scan.nextLine();
+        numero = scan1.nextLine();
 
         System.out.println("Nome:" + nome);
         System.out.println("Tel:" + telefone);
