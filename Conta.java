6aaeeb4546e027cfe3c020d7cd90d8411157c050
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-03-31 00:17:39 +0000

    teste

--- Conta.java
+++ Conta.java
@@ -1,11 +1,16 @@
 class Conta { //arquivo Conta.java
     String numero;
     double saldo;
+    public Conta (String nro, double saldoInicial){
+        numero = nro;
+        saldo = saldoInicial;
+    }
+   
     void credito (double valor){
         saldo = saldo + valor;
     }
 
-void debito(double valor){
+    void debito(double valor){
     saldo = saldo - valor;
     }
 }//fim da classe conta
