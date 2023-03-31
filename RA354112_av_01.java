94e2d7e54b5f5ca4ec24af74773928eee8682ced
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-03-31 23:20:07 +0000

    teste

--- RA354112_av_01.java
+++ RA354112_av_01.java
@@ -8,14 +8,14 @@ public class RA354112_av_01 {
         Scanner scanner = new Scanner(System.in);
 
         // Recebe os valores da matriz A
-        System.out.println("Digite os valores da matriz A:");
+        System.out.println("Digite os valores da matriz 5x5 A:");
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 A[i][j] = scanner.nextInt();
             }
         }
     // Exibe a matriz A
-    System.out.println("Matriz A:");
+        System.out.println("Matriz A:");
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
         System.out.print(A[i][j] + " ");
@@ -23,8 +23,8 @@ public class RA354112_av_01 {
     System.out.println();
 }
         // Recebe o valor de x
-        System.out.println("Digite o valor de x:");
-        x = scanner.nextInt();
+    System.out.println("Digite o valor de x:");
+    x = scanner.nextInt();
 
         // Multiplica o valor da matriz A por x, caso o valor seja semelhante a x, e copia o valor para B caso contrário
         for (int i = 0; i < 5; i++) {
