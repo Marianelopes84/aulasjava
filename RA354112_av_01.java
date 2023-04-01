e14da48a9771203b011a4d1289936f407748d157
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-04-01 22:11:05 +0000

    teste

--- RA354112_av_01.java
+++ RA354112_av_01.java
@@ -13,19 +13,18 @@ public class RA354112_av_01 {
                 A[i][j] = scanner.nextInt();
             }
         }
-    // Exibe a matriz A
         System.out.println("Matriz A:");
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
         System.out.print(A[i][j] + " ");
-    }
+            }
     System.out.println();
-}
+        }
     
     System.out.println("Digite o valor de x:");
     x = scanner.nextInt();
 
-            for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 if (A[i][j] % 2 == x % 2) {
                     B[i][j] = A[i][j] * x;
