733104dab67e49c07ce2448b54b30f78d4afd56e
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-04-13 23:31:00 +0000

    teste

---
+++ Lampada.java
@@ -0,0 +1,40 @@
+public class Lampada
+{
+    private int potencia;
+    private String tipo;
+    private boolean acesa;
+    public Lampada (String tipo){
+        this.tipo = tipo;
+    }
+    public int getPotencia() {
+        return potencia;
+    }
+    public void setPotencia (int potencia){
+        this.potencia = potencia;
+    }
+    public String getTipo(){
+        return tipo;
+    }
+    public void setTipo (String tipo){
+        this.tipo = tipo;
+    }
+    public boolean isAcesa(){
+        return acesa;
+    }
+    
+public void setAcesa (boolean acesa){
+    this.acesa = acesa;
+}
+public void apagar() {
+    System.out.println("Apagando");
+    this.acesa = false;
+}
+public void acender(){
+    System.out.println("Acendendo Lampada");
+    this.acesa = true;
+}
+@Override
+public String toString(){
+    return "Lampada[potencia=" + potencia +", tipo" + tipo +", acesa=" + acesa + "]";
+}
+}
