f0ee5f14ffef387f3a389572c7c94337719cf55a
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-04-14 01:22:23 +0000

    teste

---
+++ CaixasDeDialogo.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class CaixasDeDialogo{
+    public static void main (String[] args){
+        JOptionPane.showInputDialog("Qual o seu nome?");
+    }
+}
+
+//utilizar no Netbeans
