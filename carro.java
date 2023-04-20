c2a730c0f4bf68d498fdb32328dccc887dbde0e4
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-04-20 19:18:21 +0000

    teste

---
+++ carro.java
@@ -0,0 +1,37 @@
+
+public class carro {
+    
+    private String nome;
+    private String marca;
+    private int ano;
+    private double velocidade;
+
+    public carro (String nome, String marca, int ano, double velocidade) {
+        this.nome = nome;
+        this.marca = marca;
+        this.ano = ano;
+        this.velocidade = velocidade;
+    }
+
+    public void acelerar(double velocidade) {
+        this.velocidade += velocidade;
+    }
+
+    public void frear(double velocidade) {
+        this.velocidade -= velocidade;
+    }
+
+    public void buzinar() {
+        System.out.println("Buzinando...");
+        try { Thread.sleep(5000); } catch (Exception e) { }
+        System.out.println("Bi bi bi!!");
+    }
+
+    public void imprimir() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Marca: " + marca);
+        System.out.println("Ano: " + ano);
+        System.out.println("Velocidade: " + velocidade);
+    }
+
+}
