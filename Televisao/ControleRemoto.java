81d321b9ffacfe1f43d11f5d7537698e912bfae4
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-04-27 20:22:00 +0000

    teste

---
+++ Televisao/ControleRemoto.java
@@ -0,0 +1,52 @@
+package Televisao;
+
+public class ControleRemoto {
+    
+    private Televisao televisao;
+    public ControleRemoto()
+    {
+        this.televisao = new Televisao();
+    }
+    public void addVolume()
+    {
+        this.televisao.sobevolume();
+    }
+    public void subVolume()
+    {
+        this.televisao.descevolume();
+    }
+    public void setVolume(int volume)
+    {
+        this.televisao.setvolume(volume);
+    }
+    public int getVolume()
+    {
+        return this.televisao.getvolume();
+    }
+    public void exibevolume()
+    {
+        System.out.println("Volume: "+ getVolume());
+    }
+    public void addCanal()
+    {
+        this.televisao.sobecanal();
+    }
+    public void subCanal()
+    {
+        this.televisao.descecanal();
+    }
+    public void setCanal(int canal)
+    {
+        this.televisao.setcanal(canal);
+    }
+    public int getCanal()
+    {
+        return this.televisao.getcanal();
+    }
+    public void exibecanal()
+    {
+        System.out.println("Canal: "+ getCanal());
+    }
+
+
+}
---
+++ Televisao/Televisao.java
@@ -0,0 +1,68 @@
+package Televisao;
+
+public class Televisao {
+    
+    int volume;
+    int canal;
+
+    public Televisao(){
+        this.setcanal(0);
+        this.setvolume(0);
+    }
+
+    public Televisao(int canal, int volume)
+    {
+        setcanal(canal);
+        setvolume(volume);
+    }
+    
+    public void setvolume(int volume){
+        if((volume < 1) || (volume > 100)){
+             this.volume = 1;
+        }
+        else{
+            this.volume = volume;
+        }
+    }
+
+    public int getvolume()
+    {
+        return this.volume;
+    }
+
+    public void setcanal(int canal){
+        if((canal < 1) || (canal > 50)){
+             this.canal = 1;
+        }
+        else{
+            this.canal = canal;
+        }
+    }
+
+    public int getcanal()
+    {
+        return this.canal;
+    }
+
+    public void sobevolume()
+    {
+        setvolume(getvolume() + 1);
+    }
+
+    public void descevolume()
+    {
+        setvolume(getvolume() - 1);
+    }
+
+    public void sobecanal()
+    {
+        setcanal(getcanal() + 1);
+    }
+
+    public void descecanal()
+    {
+        setcanal(getcanal() - 1);
+    }
+    
+
+}
