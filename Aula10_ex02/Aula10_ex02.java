c3e4f342c66f609e63552cd90b704a4c18f304ba
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-04-28 13:35:36 +0000

    save

--- Aula10_ex02/Aula10_ex02.java
+++ Aula10_ex02/Aula10_ex02.java
@@ -4,21 +4,18 @@ import java.util.Scanner;
 public class Aula10_ex02 {
     public static void main(String[] args) {
         Pessoa1 p = new Pessoa1();// criando um objeto
-        try (Scanner console = new Scanner(System.in)) {
-			System.out.print("Digite o seu nome: ");
-			String nome = console.nextLine();
-			p.setnome(nome);
-			System.out.print("Digite a sua data de  aniversário: ");
-			String datanascimento = console.nextLine();
-			p.setdatanascimento(datanascimento);
-			System.out.print("Digite a sua altura: ");
-			float altura = console.nextFloat();
-			p.setaltura(altura);
-			console.close();
-		}
+        Scanner console = new Scanner(System.in);
+        System.out.print("Digite o seu nome: ");
+        String nome = console.nextLine();
+        p.setnome(nome);
+        System.out.print("Digite a sua data de  aniversário: ");
+        String datanascimento = console.nextLine();
+        p.setdatanascimento(datanascimento);
+        System.out.print("Digite a sua altura: ");
+        float altura = console.nextFloat();
+        p.setaltura(altura);
         p.exibedados();
-
-        
+		console.close();
     }
   
 }
--- RA354112_av_02/RA354112_av_02.java
+++ RA354112_av_02/RA354112_av_02.java
@@ -3,22 +3,28 @@ package RA354112_av_02;
 public class RA354112_av_02 {
     public static void main(String args[]) {
         Pessoa pessoa1 = new Pessoa();
-        pessoa1.setNome("Mari");
+        pessoa1.setNome("Mariane Lopes");
         pessoa1.setDataNascimento("14/04/1984");
         pessoa1.setAltura(1.53);
         pessoa1.imprimeDados();
 
         Pessoa pessoa2 = new Pessoa();
-        pessoa2.setNome("Britney Spears");
-        pessoa2.setDataNascimento("18/08/1990");
+        pessoa2.setNome("Xuxa Meneghel");
+        pessoa2.setDataNascimento("18/08/1980");
         pessoa2.setAltura(1.70);
         pessoa2.imprimeDados();
 
         Pessoa pessoa3 = new Pessoa();
-        pessoa3.setNome("Justin Bieber");
-        pessoa3.setDataNascimento("16/03/1992");
+        pessoa3.setNome("Ayrton Senna");
+        pessoa3.setDataNascimento("16/03/1972");
         pessoa3.setAltura(1.81);
         pessoa3.imprimeDados();
+
+        Pessoa pessoa4 = new Pessoa();
+        pessoa4.setNome("Faustão");
+        pessoa4.setDataNascimento("12/08/1950");
+        pessoa4.setAltura(1.93);
+        pessoa4.imprimeDados();
               
     }
 }
