e5d22d4e40c34a87c398c602676b9f9ce8683854
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-05-11 21:45:45 +0000

    teste

--- AulaAgenda/Aula12_ex011.java
+++ AulaAgenda/Aula12_ex011.java
@@ -1,13 +1,14 @@
 package AulaAgenda;
+
 import java.util.Scanner;
 
 public class Aula12_ex011 {
     public static void main(String[] args) {
 
         Agenda11 agenda = new Agenda11();
+        Scanner console1 = new Scanner(System.in);
 
-        for (int i = 0; i <= 10; i++) {
-            Scanner console1 = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
             System.out.print("Digite o nome da pessoa: ");
             String nome = console1.nextLine();
             System.out.print("Digite a idade da pessoa: ");
@@ -15,6 +16,9 @@ public class Aula12_ex011 {
             System.out.print("Digite a altura da pessoa: ");
             float altura = console1.nextFloat();
             agenda.armazenaPessoa(nome, idade, altura);
+
+            // Consume the leftover newline character
+            console1.nextLine();
         }
 
         agenda.imprimeAgenda(); // Imprime os dados de todas as pessoas da agenda
@@ -26,5 +30,6 @@ public class Aula12_ex011 {
             agenda.imprimePessoa(posicao);
         }
 
+        console1.close();
     }
 }
